9fbb21c4e5a9c9d9c136144f8a7318b3075abb61
Steven <s.koerts2@gmail.com>
2023-10-29 19:44:42 +0100

    Add counter

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import CounterView from '../views/CounterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +9,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => HomeView
     },
     {
       path: '/about',
@@ -15,8 +17,14 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => AboutView
+    }, 
+    {
+      path: '/counter',
+      name: 'counter',
+      component: () => CounterView
     }
+
   ]
 })
 
---
+++ app/src/views/CounterView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Counter from '../components/Counter.vue';
+</script>
+
+<template>
+  <div class="counter">
+    <h1>This is the counter page</h1>
+
+    <Counter/>
+
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .counter {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
