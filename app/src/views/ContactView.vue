11447d2f061af015b44081b17e3ededab26de158
Steven24K <s.koerts2@gmail.com>
2023-11-03 16:28:42 +0100

    add watcher to validate form

--- app/src/views/ContactView.vue
+++ app/src/views/ContactView.vue
@@ -2,12 +2,45 @@
 import { ref } from 'vue'
 
 export default {
-  props:[],
+  props: [],
   data() {
     return {
-       name: '', 
-       email: '', 
-       msg: '', 
+      name: '',
+      email: '',
+      msg: '',
+      errors: new Map()
+    }
+  },
+
+  watch: {
+    email() {
+      const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+      // Test the email against the regular expression
+      const isEmail = emailRegex.test(this.email);
+      if (isEmail || this.email === '') {
+        this.errors.delete('email')
+      } else {
+        this.errors.set('email', 'Please provide a valid emailadress')
+      }
+    },
+
+    name() {
+      if (this.name.length > 25) {
+        this.errors.set('name', 'Name is to long')
+      } else if (this.name.length == 0) {
+        this.errors.set('name', 'Name is required')
+      } else {
+        this.errors.delete('name')
+      }
+    }, 
+
+    msg() {
+      if (this.msg.length > 0 && this.msg.length <= 250) {
+        this.errors.delete('msg')
+      } else {
+        this.errors.set('msg', 'Message must be longer than 0 and smaller 250 characters')
+      }
     }
   },
 
@@ -24,15 +57,18 @@ export default {
 
   methods: {
     submitForm(event) {
-        alert(`
+      alert(`
         You submited the form!!!
         Name: ${this.name}
         Email: ${this.email}
         Msg: ${this.msg}
         `)
-    }, 
+    },
     setValue(event, key) {
-        this[key] = event.target.value
+      this[key] = event.target.value
+    },
+    formValid(){
+      return this.errors.size == 0 && this.name !== '' && this.email != '' && this.msg != ''
     }
   }
 
@@ -42,14 +78,18 @@ export default {
 <template>
   <div class="playground">
     <h2>Contact</h2>
+    <h4>There are {{ errors.size }} errors</h4>
+    <ul>
+      <li v-for="error in Array.from(errors.values())">{{ error }}</li>
+    </ul>
     <form v-on:submit.prevent="submitForm">
-        Name: <input v-on:input="setValue($event, 'name')" v-bind:value="name"/>
-        Email: <input v-on:input="setValue($event, 'email')" v-bind:value="email"/>
-        Message: <textarea v-on:input="setValue($event, 'msg')" v-bind:value="msg" cols="50" rows="10"></textarea>
-        <button>Submit</button>
+      Name: <input v-on:input="setValue($event, 'name')" v-bind:value="name" />
+      Email: <input v-on:input="setValue($event, 'email')" v-bind:value="email" />
+      Message: <textarea v-on:input="setValue($event, 'msg')" v-bind:value="msg" cols="50" rows="10"></textarea>
+      <button :disabled="!formValid()">Submit</button>
     </form>
 
-    Output: 
+    Output:
     <p v-html="form"></p>
   </div>
 </template>
@@ -62,14 +102,15 @@ export default {
 }
 
 button {
-    background-color: rgb(92, 158, 92);
-    border: none; 
-    width: 30%;
-    height: 30px;
-    margin: 15px;
+  background-color: rgb(92, 158, 92);
+  border: none;
+  width: 30%;
+  height: 30px;
+  margin: 15px;
 }
 
-.playground input, textarea {
-    display: block;
+.playground input,
+textarea {
+  display: block;
 }
 </style>
