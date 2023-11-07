50ecb244d903a2aed46873c16578ab75d6d3eb01
Steven24K <s.koerts2@gmail.com>
2023-11-07 10:25:51 +0100

    Add component

--- vue-cli-app/src/App.vue
+++ vue-cli-app/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <h2>My friends:</h2>
+  <ul>
+    <friend-contact v-for="friend in friends" :key="friend.id" :id="friend.id" :name="friend.name" :phone="friend.phone" :email="friend.email"></friend-contact>
+  </ul>
+
+  <HelloWorld msg="Welcome to Your Vue.js App!" />
 </template>
 
 <script>
@@ -8,6 +14,24 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      friends: [
+        {
+          id: 'Manuel',
+          name: 'Manuel Lorenz',
+          phone: '01233456678',
+          email: 'info@sample.com'
+        },
+        {
+          id: 'Lucy', 
+          name: 'Lucy blackstone', 
+          phone: '3242343243', 
+          email: 'infor@lucy.com'
+        }
+      ]
+    }
+  },
   components: {
     HelloWorld
   }
---
+++ vue-cli-app/src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggle">{{ visible ? 'Hide' : 'Show' }} Details</button>
+
+
+        <ul v-if="visible">
+            <li>{{ friend.phone }}</li>
+            <li>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+  
+<script>
+
+export default {
+    props: ["id", "name", "phone", "email"],
+    data(props) {
+        return {
+            visible: false,
+            friend: {
+                id: props.id,
+                name: props.name,
+                phone: props.phone,
+                email: props.email
+            }
+        }
+    },
+    methods: {
+        toggle() {
+            this.visible = !this.visible
+        }
+    }
+}
+</script>
+  
+<style></style>
+  
